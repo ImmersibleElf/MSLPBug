3e5a6cce3153eec135a3538c43551d7b7f12e90c
ImmersibleElf <chrisward714@gmail.com>
2016-03-23 13:47:49 +0800

    Initial commit.

---
+++ app/src/main/java/com/immersibleelf/mslpbug/SettingsActivity.java
@@ -0,0 +1,33 @@
+package com.immersibleelf.mslpbug;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the settings from an XML resource
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
+}
